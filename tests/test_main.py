90d2f67f74287d965d1666dca938697a2f7c47cb
chenashkenazi <chenashkenazi95@gmail.com>
2024-09-08 01:46:17 +0300

    cleaning

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.main import app
 from app.database import get_db, Base
-from app import schemas
 import random
 
 
